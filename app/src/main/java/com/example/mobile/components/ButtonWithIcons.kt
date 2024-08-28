c0f0de5bb784f811280ab2526eafc2c3f37a6de8
FranManfredi <FManfredi@mail.austral.edu.ar>
2024-08-28 14:46:10 -0300

    remove unused class

--- app/src/main/java/com/example/mobile/components/ButtonWithIcons.kt
+++ app/src/main/java/com/example/mobile/components/ButtonWithIcons.kt
@@ -1,44 +1,17 @@
 package com.example.mobile.components
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import com.example.mobile.ui.theme.orange
-
-@Composable
-fun ButtonWithIconsSample(
-    leadingIcon: ImageVector,
-    title: String,
-    trailingIcon: ImageVector,
-) {
-    Button(onClick = {}, modifier = Modifier
-        .fillMaxWidth()
-        .wrapContentHeight()) {
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Icon(imageVector = leadingIcon, contentDescription = "")
-            Text(text = title)
-            Icon(imageVector = trailingIcon, contentDescription = "")
-        }
-    }
-}
 
 @Composable
 fun ButtonWithIcon(
